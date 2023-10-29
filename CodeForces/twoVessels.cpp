48b24eca622f272af8a9041f1d1d805b5f0c6943
Aidos <bizzar@MacBook-Air--Aidos.local>
2023-10-29 18:54:12 +0600

    first commit

---
+++ CodeForces/twoVessels.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define vi vector<int>
+#define vll vector<long long>
+
+void solve() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    double n = (a+b)/2;
+    double v = abs(a-n)/c;
+    int ans = ceil(v);
+    cout << ans  << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t; // Uncomment this line if there is a test case count
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
